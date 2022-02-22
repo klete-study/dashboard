e3b894039f900c2aa75391cfd7fd24bc55b3488c
klete-study <canzasecity5@naver.com>
2022-02-22 16:56:27 +0900

    boolean 사용, if문 true 삭제, 즉시실행함수 삭제

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,219 +1,179 @@
-(function () {
-  const button = document.getElementById('button');
-  const borderFontColor = document.getElementById('borderColor'); 
-  const select = document.getElementById('colorSelectBox');
-  const dropdownMenu = document.getElementById('dropdownSettingMenu');
-  const text = document.getElementsByClassName('tag');
+const borderFontColor = document.getElementById('borderColor'); 
+const select = document.getElementById('colorSelectBox');
+const button = document.querySelector('.button');
+const dropdownMenuColor = document.getElementById('dropdownSettingMenu');
+const text = document.getElementsByClassName('tag');
 
-  const colorUpdate = (value) => {
-    for (let index = 0; index < select.length; index++) {
-      if (select[index].value == value) {
-        if (borderFontColor.checked == true) {
-          button.classList.add(`${value}-change`);
-          button.classList.remove(`${value}-hover-change`);
-        } else {
-          button.classList.add(value);
-          button.classList.remove(`${select[index].value}-hover`);
-        }
-        dropdownMenu.classList.add(value);
+
+const colorUpdate = (value) => {
+  for (let index = 0; index < select.length; index++) {
+    if (select[index].value == value) {
+      if (borderFontColor.checked) {
+        button.classList.add(`${value}-change`);
+        button.classList.remove(`${value}-hover-change`);
       } else {
+        button.classList.add(value);
         button.classList.remove(`${select[index].value}-hover`);
-        button.classList.remove(select[index].value);
-        dropdownMenu.classList.remove(select[index].value);
       }
+      dropdownMenuColor.classList.add(value);
+    } else {
+      button.classList.remove(`${select[index].value}-hover`);
+      button.classList.remove(select[index].value);
+      dropdownMenuColor.classList.remove(select[index].value);
     }
   }
-  
-  const hoverChangeElement = (value) => {
+}
+
+const hoverChangeElement = (value) => {
+  for (let index = 0; index < select.length; index++) {
+    if (select[index].value == value) {
+      if (borderFontColor.checked) {
+        if (value == 'default') {
+          break;
+        }
+        button.classList.add(`${value}-hover-change`);
+        button.classList.remove(`${value}-change`);
+      } else {
+        button.classList.add(`${value}-hover`);
+      }
+      button.classList.remove(value);
+    } else {
+      button.classList.remove(`${select[index].value}-hover`);
+    }
+  }
+};
+
+const changeBorderFontColor = (value, check) => {
+  if (check) {  
     for (let index = 0; index < select.length; index++) {
       if (select[index].value == value) {
-        if (borderFontColor.checked == true) {
-          if (value == 'default') {
-            break;
-          }
-          button.classList.add(`${value}-hover-change`);
-          button.classList.remove(`${value}-change`);
-        } else {
-          button.classList.add(`${value}-hover`);
-        }
+        button.classList.add(`${value}-change`);
         button.classList.remove(value);
+        text[0].innerText = 'Yes';
+        text[0].style.backgroundColor = '#dddddd';
+        dropdownMenuColor.classList.add(value);
       } else {
-        button.classList.remove(`${select[index].value}-hover`);
-      }
+        button.classList.remove(`${select[index].value}-change`);
+        dropdownMenuColor.classList.remove(select[index].value);
+      } 
     }
-  };
+  } else {
+    button.classList.add(value);
+    button.classList.remove(`${value}-change`);
+    text[0].innerText = 'No';
+    text[0].style.backgroundColor = '#eeeeee';
+  }
+};
 
-  const changeBorderFontColor = (value, check) => {
-    if (check == true) {  
-      for (let index = 0; index < select.length; index++) {
-        if (select[index].value == value) {
-          button.classList.add(`${value}-change`);
-          button.classList.remove(value);
-          text[0].innerText = 'Yes';
-          text[0].style.backgroundColor = '#dddddd';
-          dropdownMenu.classList.add(value);
-        } else {
-          button.classList.remove(`${select[index].value}-change`);
-          dropdownMenu.classList.remove(select[index].value);
-        } 
-      }
+select.addEventListener('change', (event) => {
+  if (borderFontColor.checked == false) {
+    colorUpdate (event.target.value);
+  } else {
+    changeBorderFontColor(select.value, borderFontColor.checked);
+  }
+});
+
+button.addEventListener('mouseover', () => {
+  if (button.classList.contains('disabled') == false) {
+    hoverChangeElement(select.value);
+    button.classList.add('hover');
+  }
+});
+
+button.addEventListener('mouseout', () => {
+  colorUpdate(select.value);
+  button.classList.remove('hover');
+});
+
+borderFontColor.addEventListener('change', (event) =>{
+  changeBorderFontColor(select.value, event.target.checked);
+});
+
+const disabled = document.getElementById('disabled');
+
+disabled.addEventListener('change', (event) => {
+  disabledElement(event.target.checked);
+});
+
+const changeBorderLine = (check) => {
+  if (check) {
+    button.classList.remove('border-white');
+    text[1].innerText = 'Yes';
+    text[1].style.backgroundColor = '#dddddd'
+  } else {
+    button.classList.add('border-white');
+    text[1].innerText = 'No';
+    text[1].style.backgroundColor = '#eeeeee';
+  }
+};
+
+const borderLine = document.getElementById('borderLine'); 
+
+borderLine.addEventListener('change', (event) => {
+  changeBorderLine(event.target.checked);
+});
+
+const block = document.getElementById('block'); 
+
+block.addEventListener('change', (event) => {
+  const buttonDisplay = document.querySelector('.buttonDisplay');
+  if (event.target.checked) {
+    buttonDisplay.classList.add('block');
+    text[3].innerText = 'Yes';
+    text[3].style.backgroundColor = '#dddddd';
+  } else {
+    buttonDisplay.classList.remove('block');
+    text[3].innerText = 'No';
+    text[3].style.backgroundColor = '#eeeeee';
+  }
+  const width = document.querySelector('body');
+  const buttonWidth = document.querySelector('.button').offsetWidth;
+  width.style.setProperty('--left', `${changeDropdownPos(pos, buttonWidth)}px`);
+});
+
+
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+const disabledElement = (check) => {
+  if (check) {
+    button.classList.add('disabled');
+    dropdownMenu.classList.add('displayNone');
+    dropdownMenu.classList.remove('block');
+    text[2].innerText = 'Yes';
+    text[2].style.backgroundColor = '#dddddd';
+  } else {
+    button.classList.remove('disabled');
+    if (button.classList.contains('click')) {
+      dropdownMenu.classList.add('block');
+      dropdownMenu.classList.remove('displayNone');
+    }
+    text[2].innerText = 'No';
+    text[2].style.backgroundColor = '#eeeeee';
+  }
+};
+
+button.addEventListener('click', () => {
+  if (disabled.checked == false){
+    if (dropdownMenu.classList.contains('displayNone')) {
+      button.classList.add('click');
+      dropdownMenu.classList.add('block');
+      dropdownMenu.classList.remove('displayNone');
     } else {
-      button.classList.add(value);
-      button.classList.remove(`${value}-change`);
-      text[0].innerText = 'No';
-      text[0].style.backgroundColor = '#eeeeee';
-    }
-  };
-
-  select.addEventListener('change', (event) => {
-    if (borderFontColor.checked == false) {
-      colorUpdate (event.target.value);
-    } else {
-      changeBorderFontColor(select.value, borderFontColor.checked);
-    }
-  });
-
-  button.addEventListener('mouseover', (event) => {
-    if (button.classList.contains('disabled') == false){
-      hoverChangeElement(select.value);
-      button.classList.add('hover');
-    }
-  });
-  
-  button.addEventListener('mouseout', (event) => {
-    colorUpdate(select.value);
-    button.classList.remove('hover');
-  });
-
-  borderFontColor.addEventListener('change', (event) =>{
-    changeBorderFontColor(select.value, event.target.checked);
-  });
-})();
-
-
-(function () {
-  const button = document.getElementById('button');
-  const text = document.getElementsByClassName('tag');
-  
-  const disabeldElement = (check) => {
-    if (check == true) {
-      button.classList.add('disabled');
-      text[2].innerText = 'Yes';
-      text[2].style.backgroundColor = '#dddddd';
-    } else {
-      button.classList.remove('disabled');
-      text[2].innerText = 'No';
-      text[2].style.backgroundColor = '#eeeeee';
-    }
-  };
-  
-  const disabeld = document.getElementById('disabled');
-  disabeld.addEventListener('change', (event) => {
-    disabeldElement(event.target.checked);
-  });
-
-})();
-
-(function () {
-  const button = document.getElementById('button');
-  const text = document.getElementsByClassName('tag');
-  const changeBorderLine = (check) => {
-    if (check == true) {
-      button.classList.remove('border-white');
-      text[1].innerText = 'Yes';
-      text[1].style.backgroundColor = '#dddddd'
-    } else {
-      button.classList.add('border-white');
-      text[1].innerText = 'No';
-      text[1].style.backgroundColor = '#eeeeee';
-    }
-  };
-  
-  const borderLine = document.getElementById('borderLine'); 
-  borderLine.addEventListener('change', (event) => {
-    changeBorderLine(event.target.checked);
-  });
-  
-})();
-
-(function () {
-  const block = document.getElementById('block'); 
-  const text = document.getElementsByClassName('tag');
-  const buttonContainer = document.getElementById('buttonContainer');
-
-  block.addEventListener('change', (event) => {
-    if (event.target.checked == true) {
-      buttonContainer.classList.add('block');
-      text[3].innerText = 'Yes';
-      text[3].style.backgroundColor = '#dddddd';
-    } else {
-      buttonContainer.classList.remove('block');
-      text[3].innerText = 'No';
-      text[3].style.backgroundColor = '#eeeeee';
-    }
-    const width = document.querySelector('body');
-    const buttonWidth = document.querySelector('#button').offsetWidth;
-    width.style.setProperty('--left', `${changeDropdownPos(pos, buttonWidth)}px`);
-  });
-})();
-
-(function () {
-  const button = document.getElementById('button');
-  const text = document.getElementsByClassName('tag');
-  const dropdownMenu = document.getElementById('dropdownMenu');
-  
-  const disabeldElement = (check) => {
-    if (check == true) {
-      button.classList.add('disabled');
+      button.classList.remove('click');
       dropdownMenu.classList.add('displayNone');
       dropdownMenu.classList.remove('block');
-      text[2].innerText = 'Yes';
-      text[2].style.backgroundColor = '#dddddd';
-    } else {
-      button.classList.remove('disabled');
-      if (button.classList.contains('click') == true) {
-        dropdownMenu.classList.add('block');
-        dropdownMenu.classList.remove('displayNone');
-      }
-      text[2].innerText = 'No';
-      text[2].style.backgroundColor = '#eeeeee';
     }
-  };
-  
-  const disabeld = document.getElementById('disabled');
-  disabeld.addEventListener('change', (event) => {
-    disabeldElement(event.target.checked);
-  });
-})();
-
-(function() {
-  const button = document.getElementById('button');
-  const dropdown = document.getElementById('dropdownMenu');
-  const disabeld = document.getElementById('disabled');
-
-  button.addEventListener('click', (event) => {
-    if (disabeld.checked == false){
-      if (dropdown.classList.contains('displayNone') == true) {
-        button.classList.add('click');
-        dropdown.classList.add('block');
-        dropdown.classList.remove('displayNone');
-      } else {
-        button.classList.remove('click');
-        dropdown.classList.add('displayNone');
-        dropdown.classList.remove('block');
-      }
-    }
-  });
-
-  const cencelButton = document.getElementById('cancelText');
-  cencelButton.onclick = function (event) {
-    button.classList.remove('click');
-    dropdown.classList.add('displayNone');
-    dropdown.classList.remove('block');
   }
-  
-})();
+});
 
+const cencelButton = document.querySelector('.cancelText');
+
+cencelButton.addEventListener ('click', () => {
+  button.classList.remove('click');
+  dropdownMenu.classList.add('displayNone');
+  dropdownMenu.classList.remove('block');
+});
+  
 let pos = 'center';
 let dropdownWidth = 0; 
 
@@ -235,14 +195,15 @@ const widthValue = (value) => {
 function changeLevelAndWider (event) {
   const value = event.target.value;
   const levelName = document.getElementsByName('level');
+  const leveltext = document.getElementsByClassName('levelText');
   const widerName = document.getElementsByName('wider');
-  const text = document.getElementsByClassName('text');
+  const widertext = document.getElementsByClassName('widerText');
   
   if (event.path[0].name == 'level') {
     for (let index = 0; index < levelName.length; index++) {
       if (widerName[index].value == value) {
         button.classList.add(`${value}-padding`);   
-        text[0].innerText = value;
+        leveltext.innerText = value;
       } else {
           button.classList.remove(`${widerName[index].value}-padding`);
         }
@@ -251,22 +212,21 @@ function changeLevelAndWider (event) {
   
   else if (event.path[0].name == 'wider') {
     for (let index = 0; index < widerName.length; index++) {
-      if (widerName[index].value == value) {
+      if (levelName[index].value == value) {
         button.classList.add(`${value}-fontSize`);
-        text[1].innerText = value;
+        widertext.innerText = value;
       } else {
-          button.classList.remove(`${widerName[index].value}-fontSize`);
+          button.classList.remove(`${levelName[index].value}-fontSize`);
       }
     }
   }
   
   const width = document.querySelector('body');
-  const buttonWidth = document.querySelector('#button').offsetWidth;
+  const buttonWidth = document.querySelector('.button').offsetWidth;
   width.style.setProperty('--left', `${changeDropdownPos(pos, buttonWidth)}px`);
 };
 
 const dropdownName = document.getElementsByName('dropdown');
-const dropdownMenu = document.getElementById('dropdownMenu');
 
 const widthArray = [
   260,
@@ -276,9 +236,9 @@ const widthArray = [
 
 function changeDropdownWider (event) {
   const value = event.target.value;
-  const text = document.getElementsByClassName('text')[2];
+  const text = document.getElementsByClassName('dropdownWiderText');
   const width = document.querySelector('body');
-  const buttonWidth = document.querySelector('#button').offsetWidth;
+  const buttonWidth = document.querySelector('.button').offsetWidth;
 
   for (let index = 0; index < dropdownName.length; index++) {
     if (dropdownName[index].value == value) {
@@ -290,12 +250,11 @@ function changeDropdownWider (event) {
   }
 };
 
-
 function changePosition (event) {
   const value = event.target.value;
-  const text = document.getElementsByClassName('text')[3];
+  const text = document.getElementsByClassName('dropdownPosText');
   const width = document.querySelector('body');
-  const buttonWidth = document.querySelector('#button').offsetWidth
+  const buttonWidth = document.querySelector('.button').offsetWidth
   const dropdownPos = document.getElementsByName('dropdownPos');
   
   for (let index = 0; index < dropdownPos.length; index++) {
@@ -310,15 +269,16 @@ function changePosition (event) {
   }
 };
 
-(function () {
-  const value = document.querySelector('body');
-  const buttonWidth = document.querySelector('#button').offsetWidth;
-  window.onload = function(event) {
-    dropdownWidth = widthArray[2];
-    value.style.setProperty('--left', `${changeDropdownPos(pos, dropdownWidth)}px`);
-  };
-  
-  window.onresize = function(event) {  
-    value.style.setProperty('--left', `${changeDropdownPos(pos, buttonWidth)}px`);
-  };
-})();
+
+const body = document.querySelector('body');
+
+window.onload = function() {
+  dropdownWidth = widthArray[2];
+  body.style.setProperty('--left', `${changeDropdownPos(pos, dropdownWidth)}px`);
+};
+
+window.onresize = function() {  
+  const buttonWidth = document.querySelector('.button').offsetWidth;
+  body.style.setProperty('--left', `${changeDropdownPos(pos, buttonWidth)}px`);
+};
+
--- js/timeline.js
+++ js/timeline.js
@@ -1,147 +1,142 @@
-(function () {
+const timelineArray = [ ];
 
-  const timelineArray = [];
-  
-  const timelinekeyArray = [
-    'time',
-    'color',
-    'title',
-    'description'
-  ];
+const timelinekeyArray = [
+  'time',
+  'color',
+  'title',
+  'description'
+];
 
-  const colorArray = [
+const colorArray = [ ];
 
-  ];
+const save = document.getElementById('save');
+const show = document.getElementById('show');
+const reset = document.getElementById('reset');
+const resetTimeline = document.getElementById('resetTimeline');
+const showColor = document.getElementById('useColor');
 
-  const save = document.getElementById('save');
-  const show = document.getElementById('show');
-  const reset = document.getElementById('reset');
-  const resetTimeline = document.getElementById('resetTimeline');
-  const showColor = document.getElementById('useColor');
+const hexToRgb = (hexType) => {
+  const hex = hexType.trim().replace( "#", "" );
+  const rgb = ( 3 === hex.length ) ? 
 
-  const hexToRgb = (hexType) => {
-    const hex = hexType.trim().replace( "#", "" );
-    const rgb = ( 3 === hex.length ) ? 
-    hex.match( /[a-f\d]/gi ) : hex.match( /[a-f\d]{2}/gi );
+  hex.match( /[a-f\d]/gi ) : hex.match( /[a-f\d]{2}/gi );
+  rgb.forEach(function (str, x, arr){
+      if ( str.length === 1 ) str = str + str;
+      arr[ x ] = parseInt( str, 16 );
+  }); 
+  return "rgb (" + rgb.join(", ") + ")";
+}
 
-    rgb.forEach(function (str, x, arr){
-        if ( str.length == 1 ) str = str + str;
-        arr[ x ] = parseInt( str, 16 );
-    }); 
-    return "rgb (" + rgb.join(", ") + ")";
+showColor.addEventListener('change', (event) => {
+  const colorBox = document.querySelector('.colorBox');
+
+  if (event.target.checked) {
+    colorBox.classList.remove('none');
+    for (let index = 0; index < colorArray.length; index++) {
+      const colorText = document.createElement('p');
+      colorText.innerText = `${index+1}. ${colorArray[index]}`;
+      colorBox.appendChild(colorText);
+    }
+  } else {
+    colorBox.classList.add('none');   
+    while (colorBox.childElementCount != 0) {
+      document.querySelector('.colorBox > p').remove();   
+    }
+  }
+});
+
+save.addEventListener('change', () => {
+  const count = document.querySelector('.count');
+  const time = document.getElementById('time');
+  const color = document.getElementById('color');
+  const title = document.getElementById('title');
+  const description = document.getElementById('description');
+
+  timelineArray.push([time.value,color.value,title.value,description.value]);
+  for (let index = 0; index < timelineArray[timelineArray.length-1].length-1; index++) {
+    if (timelineArray[timelineArray.length-1][index] === '' ) {
+      alert(`${timelinekeyArray[index]}을(를) 입력하지 않았습니다`)
+      timelineArray.pop();
+      return;
+    }
   }
 
-  showColor.addEventListener('change', (event) => {
-    const colorBox = document.getElementById('colorBox');
-    if (event.target.checked === true) {
-      colorBox.classList.remove('none');
-      for (let index = 0; index < colorArray.length; index++) {
-        const colorText = document.createElement('p');
-        colorText.innerText = `${index+1}. ${colorArray[index]}`;
-        colorBox.appendChild(colorText);
-      }
-    } else {
-      colorBox.classList.add('none');   
-      while (colorBox.childElementCount != 0) {
-        document.querySelector('#colorBox > p').remove();   
-      }
-    }
-  });
-  
-  save.addEventListener('change', () => {
-    const count = document.getElementById('count');
-    const time = document.getElementById('time');
-    const color = document.getElementById('color');
-    const title = document.getElementById('title');
-    const description = document.getElementById('description');
+  let addColor = false;
 
-    timelineArray.push([time.value,color.value,title.value,description.value]);
-    for (let index = 0; index < timelineArray[timelineArray.length-1].length-1; index++) {
-      if (timelineArray[timelineArray.length-1][index] === '' ) {
-        alert(`${timelinekeyArray[index]}을(를) 입력하지 않았습니다`)
-        timelineArray.pop();
-        return;
-      }
-    }
-    let code = 'yes'
-
-    for (let index = 0; index < timelineArray.length; index++) {
-      if (timelineArray[index][1] === color.value && colorArray.length != 0) {
-        code = 'no';
-        continue;
+  if (colorArray.length === 0) {
+    addColor = true
+  } else {
+    for (let index = 0; index < colorArray.length; index++) {
+      if (colorArray[index] != hexToRgb(color.value)) {
+        addColor = true;
       } else {
-        if (code === 'no') {
-          break;
-        }
-        colorArray.push(hexToRgb(color.value));
+        addColor = false;
+        break;
       }
     }
-    
-    code == 'yes'
-    count.innerText = timelineArray.length;
-    timelineArray.sort();
-    document.getElementById('timeline').reset();
-  });
+  }  
+  if (addColor) { 
+    colorArray.push(hexToRgb(color.value)); 
+  }
   
-  show.addEventListener('change', (event) => { 
-    if (event.target.checked === true) {
-      for (let arrayIndex = 0; arrayIndex < timelineArray.length; arrayIndex++) {
-        const container = document.createElement('div');
-        const textContainer = document.createElement('div');
-        container.id = 'timelineContainer'
-        textContainer.id = 'textContainer'
+  timelineArray.sort();
+  count.innerText = timelineArray.length;
+  document.querySelector('form[name="timeline"]').reset();
+});
 
-        for (let index = 0; index < timelineArray[arrayIndex].length; index++) {
-          const text = document.createElement('p');
-          text.id = timelinekeyArray[index]+arrayIndex;
-          text.className = timelinekeyArray[index];
-          if(text.className === 'title' || text.className === 'description') {
-            container.appendChild(textContainer);
-            textContainer.appendChild(text);
-
-            text.innerText = timelineArray[arrayIndex][index];
+show.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    for (let arrayIndex = 0; arrayIndex < timelineArray.length; arrayIndex++) {
+      const container = document.createElement('div');
+      const textContainer = document.createElement('div');
+      container.className = 'timelineContainer'
+      textContainer.className = 'textContainer'
+      for (let index = 0; index < timelineArray[arrayIndex].length; index++) {
+        const text = document.createElement('p');
+        
+        text.className = timelinekeyArray[index];
+        if(text.className === 'title' || text.className === 'description') {
+          container.appendChild(textContainer);
+          textContainer.appendChild(text);
+          text.innerText = timelineArray[arrayIndex][index];
+        } else {
+          if (text.className === 'color') {
+            text.style.borderColor = timelineArray[arrayIndex][index];
+            text.innerText = '';
           } else {
-
-            if (text.className === 'color') {
-              text.style.borderColor = timelineArray[arrayIndex][index];
-              text.innerText = '';
+            if (timelineArray[arrayIndex][index] < "12") {
+              text.innerText = `${timelineArray[arrayIndex][index]} AM`;
             } else {
-
-              if (timelineArray[arrayIndex][index] < "12") {
-                text.innerText = `${timelineArray[arrayIndex][index]} AM`;
-              } else {
-                text.innerText = `${timelineArray[arrayIndex][index]} PM`;
-              }
+              text.innerText = `${timelineArray[arrayIndex][index]} PM`;
             }
-            const createDiv = document.createElement('div');
-            createDiv.appendChild(text);
-            container.appendChild(createDiv);
           }
-          document.getElementById("result").appendChild(container);
+          const createDiv = document.createElement('div');
+          createDiv.appendChild(text);
+          container.appendChild(createDiv);
         }
-      }
-    } else {
-      while (document.getElementById("result").childElementCount != 0) {
-        document.querySelector('#timelineContainer').remove();     
+        document.querySelector(".timeline").appendChild(container);
       }
     }
-  });
-
-  reset.addEventListener('change', () => {
-    document.getElementById('timeline').reset();
-  });
-
-  resetTimeline.addEventListener('change', () => {
-    const count = document.getElementById('count');
-    while (document.querySelector('#timelineContainer') != null) {
-      document.querySelector('#timelineContainer').remove();
+  } else {
+    while (document.querySelector(".timeline").childElementCount != 0) {
+      document.querySelector('#timelineContainer').remove();     
     }
-    
-    while (timelineArray.length != 0) {
-      timelineArray.pop();
-      colorArray.pop();
-    }
-    count.innerText = timelineArray.length;
-  });
+  }
+});
 
-})();
+reset.addEventListener('change', () => {
+  document.querySelector('form[name="timeline"]').reset();
+});
+
+resetTimeline.addEventListener('change', () => {
+  const count = document.querySelector('.count');
+  while (document.querySelector('#timelineContainer') != null) {
+    document.querySelector('#timelineContainer').remove();
+  }
+  
+  while (timelineArray.length != 0) {
+    timelineArray.pop();
+    colorArray.pop();
+  }
+  count.innerText = timelineArray.length;
+});
