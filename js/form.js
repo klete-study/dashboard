8a5a226243c8997127f154000be878ef0e0acf4b
klete-study <canzasecity5@naver.com>
2022-02-23 19:02:06 +0900

    form validation 추가

--- js/form.js
+++ js/form.js
@@ -42,3 +42,66 @@ button.addEventListener('mouseout', () => {
   changeHoverOutColor(select.value);
   button.classList.remove('hover');
 });
+
+const idArray = [
+  email,
+  password,
+  description
+];
+
+button.addEventListener('click', () => {
+  for (let index = 0; index < idArray.length; index++) {
+    const key = idArray[index];
+    if (key.value.length <= 10) {
+      if (!key.classList.contains('bdRed')) {
+        key.classList.add('bdRed');
+        const text = document.createElement('p');
+        text.className = 'textRed';
+        text.innerText = '10글자 이상 입력하세요'
+        key.parentNode.appendChild(text);
+      }
+    } else {
+      if (key.classList.contains('bdRed')) {
+        key.classList.remove('bdRed');
+      }
+      key.parentNode.lastElementChild.remove();
+    }
+  }
+
+  const radio = document.getElementsByName('radio');
+  let radioCheck = false;
+  for (let index = 0; index < radio.length; index++) {
+    if (radio[index].checked) {
+      radioCheck = true;
+      break;
+    }
+  }
+
+  const checkbox = document.getElementsByName('checkbox');
+  let checkboxCheck = false;
+  for (let index = 0; index < checkbox.length; index++) {
+    if (checkbox[index].checked) {
+      checkboxCheck = true;
+      break;
+    }
+  } 
+
+  // const radioClass = document.querySelector('.radio')
+  // console.log(radioClass.getElementsByTagName('p'))
+  // if (!radioCheck) {
+  //   const text = document.createElement('p');
+  //   text.className = 'textRed';
+  //   text.innerText = '1개를 선택하세요';
+  //   radioClass.appendChild(text);
+  // }
+
+  // const checkboxClass = document.querySelector('.checkbox')
+  // if (!checkboxCheck) {
+  //   const text = document.createElement('p');
+  //   text.className = 'textRed';
+  //   text.innerText = '1개 이상을 선택하세요'
+  //   checkboxClass.appendChild(text);
+  // }
+
+  document.querySelector('form[name="form"]').reset();
+});
